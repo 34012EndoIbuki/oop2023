b279002b40082370e9faf5f9840110c76761cf56
遠藤　伊吹 <OIBC34012@ojs.ac.jp>
2023-04-17 11:44:57 +0900

    新しいプロジェクトの追加

---
+++ WindowsFromApp0412/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿
+namespace WindowsFromApp0412 {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.tbNum1 = new System.Windows.Forms.TextBox();
+            this.tbNum2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btAns = new System.Windows.Forms.Button();
+            this.tbAns = new System.Windows.Forms.TextBox();
+            this.nubX = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btPow = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.nubX)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tbNum1
+            // 
+            this.tbNum1.Location = new System.Drawing.Point(12, 12);
+            this.tbNum1.Multiline = true;
+            this.tbNum1.Name = "tbNum1";
+            this.tbNum1.Size = new System.Drawing.Size(100, 42);
+            this.tbNum1.TabIndex = 0;
+            // 
+            // tbNum2
+            // 
+            this.tbNum2.Location = new System.Drawing.Point(203, 12);
+            this.tbNum2.Multiline = true;
+            this.tbNum2.Name = "tbNum2";
+            this.tbNum2.Size = new System.Drawing.Size(100, 42);
+            this.tbNum2.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(147, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 27);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "+";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(345, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 27);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "=";
+            // 
+            // btAns
+            // 
+            this.btAns.Font = new System.Drawing.Font("MS UI Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btAns.Location = new System.Drawing.Point(665, 11);
+            this.btAns.Name = "btAns";
+            this.btAns.Size = new System.Drawing.Size(123, 57);
+            this.btAns.TabIndex = 3;
+            this.btAns.Text = "計算";
+            this.btAns.UseVisualStyleBackColor = true;
+            this.btAns.Click += new System.EventHandler(this.btAns_Click);
+            // 
+            // tbAns
+            // 
+            this.tbAns.Location = new System.Drawing.Point(412, 12);
+            this.tbAns.Multiline = true;
+            this.tbAns.Name = "tbAns";
+            this.tbAns.Size = new System.Drawing.Size(100, 42);
+            this.tbAns.TabIndex = 0;
+            // 
+            // nubX
+            // 
+            this.nubX.Font = new System.Drawing.Font("MS UI Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.nubX.Location = new System.Drawing.Point(12, 130);
+            this.nubX.Name = "nubX";
+            this.nubX.Size = new System.Drawing.Size(100, 34);
+            this.nubX.TabIndex = 4;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Font = new System.Drawing.Font("MS UI Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.numericUpDown1.Location = new System.Drawing.Point(203, 130);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(100, 34);
+            this.numericUpDown1.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("MS UI Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label3.Location = new System.Drawing.Point(147, 130);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 27);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "の";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("MS UI Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label4.Location = new System.Drawing.Point(329, 132);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 27);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "乗は";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(412, 122);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 42);
+            this.textBox1.TabIndex = 0;
+            // 
+            // btPow
+            // 
+            this.btPow.Font = new System.Drawing.Font("MS UI Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btPow.Location = new System.Drawing.Point(665, 117);
+            this.btPow.Name = "btPow";
+            this.btPow.Size = new System.Drawing.Size(123, 57);
+            this.btPow.TabIndex = 3;
+            this.btPow.Text = "計算";
+            this.btPow.UseVisualStyleBackColor = true;
+            this.btPow.Click += new System.EventHandler(this.btPow_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("MS UI Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label5.Location = new System.Drawing.Point(551, 132);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(75, 27);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "です。";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.nubX);
+            this.Controls.Add(this.btPow);
+            this.Controls.Add(this.btAns);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.tbAns);
+            this.Controls.Add(this.tbNum2);
+            this.Controls.Add(this.tbNum1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.nubX)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbNum1;
+        private System.Windows.Forms.TextBox tbNum2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btAns;
+        private System.Windows.Forms.TextBox tbAns;
+        private System.Windows.Forms.NumericUpDown nubX;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btPow;
+        private System.Windows.Forms.Label label5;
+    }
+}
---
+++ WindowsFromApp0412/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFromApp0412 {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void btAns_Click(object sender, EventArgs e) {
+            int num1 = int.Parse(tbNum1.Text);
+            int num2 = int.Parse(tbNum2.Text);
+            int sum = num1 + num2;
+            tbAns.Text = sum.ToString();
+        }
+
+        private void btPow_Click(object sender, EventArgs e) {
+            int num3 = int.Parse(nubX.Text);
+        }
+    }
+}
