f5775decbd57e762d1bae8e7c027699dd0bb7f7e
遠藤　伊吹 <OIBC34012@ojs.ac.jp>
2023-05-08 10:54:30 +0900

    p26まで

---
+++ Chapter01/Product/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductSample {
+
+    class Program {
+        static void Main(string[] args) {
+            Product karinto = new Product(123, "かりんとう", 180);   //インスタンス
+            Console.WriteLine("税込み価格:"+karinto.GetPriceIncludingTax());
+            Product daifuku = new Product(235, "大福もち", 160);   //インスタンス
+            Console.WriteLine("税込み価格:" + daifuku.GetPriceIncludingTax());
+
+        }
+    }
+}
