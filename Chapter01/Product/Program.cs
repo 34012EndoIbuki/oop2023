3e8f39f9c63fc2be5c36b3b078891ba197d871d2
遠藤　伊吹 <OIBC34012@ojs.ac.jp>
2023-05-08 12:09:33 +0900

    5月8日終わり

--- Chapter01/Product/Program.cs
+++ Chapter01/Product/Program.cs
@@ -18,16 +18,41 @@ namespace ProductSample {
             //Console.WriteLine("税込み価格:" + daifuku.GetPriceIncludingTax());
             #endregion
 
+            #region 
             //DateTime date = new DateTime(2023, 5, 8);
-            DateTime date = DateTime.Today;
+            DateTime datee = DateTime.Today;
 
             //10日後を求める
-            DateTime daysAfter10 = date.AddDays(10);
+            DateTime daysAfter10 = datee.AddDays(10);
             Console.WriteLine("今日の10日後は" + daysAfter10 + "日です");
 
             //10日前を求める
-            DateTime daysBefore10 = date.AddDays(-10);
+            DateTime daysBefore10 = datee.AddDays(-10);
             Console.WriteLine("今日の10日前は" + daysBefore10 + "日です");
+
+            /* 
+             * Console.WriteLine("10日後" + date.AddDays(10).Year + "年" + date.AddDays(10).Month + "月" + date.AddDays(10).Day + "日")
+             */
+            #endregion
+
+            DateTime now = DateTime.Now;
+            int date = now.Day;
+            int month = now.Month;
+            int year = now.Year;
+
+            Console.WriteLine("誕生日を入力");
+            Console.WriteLine("西暦:");
+            String bdYear = Console.ReadLine();
+            Console.WriteLine("月:");
+            String bdMonth = Console.ReadLine();
+            Console.WriteLine("日:");
+            String bdDay = Console.ReadLine();
+
+
+
+
+
+            Console.WriteLine("あなたは生まれてから今日で"　+ "日目です");
 
         }
     }
