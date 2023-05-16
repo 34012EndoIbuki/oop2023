70c87f446ec7b154690b15ab048c9300ec6be1db
遠藤　伊吹 <OIBC34012@ojs.ac.jp>
2023-05-16 10:21:07 +0900

    ReadSalesメソッドをSalesCounterクラスへ移動

--- Chapter02/売上集計プログラム/SalesCalculator/Program.cs
+++ Chapter02/売上集計プログラム/SalesCalculator/Program.cs
@@ -9,31 +9,11 @@ namespace SalesCalculator {
     class Program {
         static void Main(string[] args) {
            
-            SalesCounter sales = new SalesCounter(ReadSales(@"data\sales.csv"));
+            SalesCounter sales = new SalesCounter(SalesCounter.ReadSales(@"data\sales.csv"));
             Dictionary<string,int> amountPerStore = sales.GetPerStoreSales();
             foreach (KeyValuePair<string,int> obj in amountPerStore) {
                 Console.WriteLine("{0} {1}", obj.Key, obj.Value);
             }
-        }
-
-        //売上データを読み込み、saleオブジェクトのリストを返す
-        static List<Sale> ReadSales(string filePath) {   //データ読みこみ
-            List<Sale> sales = new List<Sale>();    //売上データ格納
-            string[] lines = File.ReadAllLines(filePath);   //ファイルからすべてのデータを読み込む
-
-            foreach (string line in lines) {    //すべての行から一行ずつ取り出す
-                string[] items = line.Split(',');   //,区切りでitemsにデータを格納　項目別に分ける
-                Sale sale = new Sale {  //saleインスタンスを生成
-                    ShopName = items[0],
-                    ProductCategory = items[1],
-                    Amount = int.Parse(items[2])
-                };
-                sales.Add(sale);    //saleインスタンスをコレクションに追加
-            }
-
-
-            return sales;
-
         }
     }
 }
