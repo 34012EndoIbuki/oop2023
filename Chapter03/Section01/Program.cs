d9cedd5e996ab0f1de4822da374e9eef7acb0b0b
遠藤　伊吹 <OIBC34012@ojs.ac.jp>
2023-05-22 12:08:42 +0900

    5/22終わり

--- Chapter03/Section01/Program.cs
+++ Chapter03/Section01/Program.cs
@@ -7,28 +7,28 @@ using System.Threading.Tasks;
 namespace Section01 {
     class Program {
 
-        
+
         static void Main(string[] args) {
             var numbers = new[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, 4 };
-            
 
-            int count = Count(numbers, delegate (int n) { return n % 2 == 0; });
+
+            int count = numbers.Count(n => n % 2 == 0);
             Console.WriteLine(count);
-            
+
         }
 
         static public bool IsEven(int n) {
             return n % 2 == 0;
         }
 
-        public static int Count(int[] numbers,Predicate<int> judge) { 
+        public static int Count(int[] numbers, Predicate<int> judge) {
             var count = 0;
             foreach (var n in numbers) {
-                if(judge(n) == true) {
+                if (judge(n) == true) {
                     count++;
                 }
             }
-        return count;
+            return count;
         }
     }
 }
