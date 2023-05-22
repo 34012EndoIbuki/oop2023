675bbd3ccafd7e54315ca55bed2b2a94a93373a7
遠藤　伊吹 <OIBC34012@ojs.ac.jp>
2023-05-22 09:36:10 +0900

    5月22日

---
+++ Chapter02/演習問題/Exercise01/Song.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    class Song {
+
+        //歌のタイトル
+        public string Title { get; set; }
+        //アーティスト名
+        public string ArtistName { get; set; }
+        //演奏時間(s)
+        public int Length { get; set; }
+
+        public Song(string title,string artistname,int lenght) {
+            Title = title;
+            ArtistName = artistname;
+            Length = lenght;
+        }
+        
+    }
+
+    
+}
