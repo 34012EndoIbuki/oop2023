bd53ed4af4b9ae9d2c23b18441c3b9fc672eae30
遠藤　伊吹 <OIBC34012@ojs.ac.jp>
2023-05-23 11:12:07 +0900

    p90まで

---
+++ Chapter03/Section03/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section03 {
+    class Program {
+        static void Main(string[] args) {
+            var list = new List<string> {
+                "Tokyo","New Delhi","Bangkok","London","Paris","Berlin","Canberra","Hong Kon",
+            };
+
+            //var exsits = list.Exists(s => s[0] =='A');
+            //Console.WriteLine(exsits);
+
+            //var name = list.Find(s => s.Length == 6);
+            //Console.WriteLine(name);
+
+            //var index = list.Find(s => s == "Canberra");
+            //Console.WriteLine(index);
+
+            //var names = list.FindAll(s => s.Length <= 5);
+            //names.ForEach(s => Console.WriteLine(s));
+
+            //var names = list.ConvertAll(s => s.ToLower());
+            //names.ForEach(s => Console.WriteLine(s));
+
+
+        }
+    }
+}
