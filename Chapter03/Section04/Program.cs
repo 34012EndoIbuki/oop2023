7cdbfc7099b1c2eb5df45fd79b2044331d9b497b
遠藤　伊吹 <OIBC34012@ojs.ac.jp>
2023-05-23 11:46:25 +0900

    section04 終

---
+++ Chapter03/Section04/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section04 {
+    class Program {
+        static void Main(string[] args) {
+
+            var names = new List<string> {
+                "Tokyo","New Delhi","Bangkok","London","Paris","Berlin","Canberra","Hong Kon",
+            };
+
+            //var query = names.Where(s => s.Length <= 5);
+            var query = names.Select(s => s.Length);
+            foreach(var s in query) {
+                Console.WriteLine(s);
+            }
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
