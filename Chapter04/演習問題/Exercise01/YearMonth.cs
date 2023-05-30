ab1263a60319479241d8f9ed2c7fb338de774819
遠藤　伊吹 <OIBC34012@ojs.ac.jp>
2023-05-30 11:26:35 +0900

    問題4.1.2

---
+++ Chapter04/演習問題/Exercise01/YearMonth.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    public class YearMonth {
+
+
+        #region 4.1.1
+        /*       private int year;
+               private int month;
+
+               public int Year {
+                   set { this.year = value; }
+                   get { return this.year; }
+               }
+
+               public int Month {
+                   set { this.month = value; }
+                   get { return this.month; }
+               }*/
+
+        public int Year { get; private set; }
+        public int Month { get; private set; }
+
+        public YearMonth(int year,int month) {
+            Year = year;
+            Month = month;
+        }
+        #endregion
+
+        #region 4.1.2
+
+        public bool Is21Century {
+            get {
+                return (2001 <= Year && Year <= 2100);
+                /*if (2001 <= Year || Year <= 2100) {
+                    return true;
+                } else {
+                    return false;
+                }*/
+            }
+            
+        }
+
+
+        #endregion
+
+
+
+    }
+}
