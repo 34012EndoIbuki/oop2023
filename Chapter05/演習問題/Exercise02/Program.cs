7635663e1632c5334d5f4173acbbe0a03061aa04
遠藤　伊吹 <OIBC34012@ojs.ac.jp>
2023-05-31 12:02:14 +0900

    c5.exercise2

---
+++ Chapter05/演習問題/Exercise02/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.Write("数字文字列:");
+            string n1 = Console.ReadLine();
+
+            Console.WriteLine(int.TryParse(n1, out int n2));
+            Console.WriteLine(string.Format("{0:#,0}", n2));
+
+
+
+        }
+    }
+}
