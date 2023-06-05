7d2c647090cb7891e745340997a5762efd550dbe
遠藤　伊吹 <OIBC34012@ojs.ac.jp>
2023-06-05 12:07:47 +0900

    6/5　終わり

--- Chapter05/演習問題/Exercise03/Program.cs
+++ Chapter05/演習問題/Exercise03/Program.cs
@@ -35,6 +35,10 @@ namespace Exercise03 {
             }
             
             Console.WriteLine("空白の数:" + count);
+
+            /*int spaces = text.Count(c => c == ' ');
+            Console.WriteLine("空白数:{0}", spaces);*/
+
         }
 
         private static void Exercise3_2(string text) {
@@ -48,6 +52,9 @@ namespace Exercise03 {
                 result = inter.Insert(index, replace);
                 Console.WriteLine(result);
             }
+
+            /*var replaced = text.Replace("big","small");
+            Console.WriteLine(replaced);*/
         }
 
         private static void Exercise3_3(string text) {
@@ -59,6 +66,10 @@ namespace Exercise03 {
                 }
             }
             Console.WriteLine(count + 1);
+
+            /*int count = text.Split(' ').Length;
+            Console.WriteLine("単語数:{0}", count);*/
+            
         }
 
         private static void Exercise3_4(string text) {
@@ -68,6 +79,10 @@ namespace Exercise03 {
                     Console.WriteLine(words);
                 }
             }
+
+            /*var words = text.Split(' ').Where(word => word.Length <= 4);
+            foreach(var word int words)
+                    Console.WriteLine(word);*/
         }
 
         private static void Exercise3_5(string text) {
@@ -83,6 +98,15 @@ namespace Exercise03 {
             }
             var NewText = sb.ToString();
             Console.WriteLine(NewText);
+
+            /*var array = text.Split(' ').ToArray();
+            var sb = new StringBuilder();
+            foreach(var word in array) {
+                sb.Append(word);
+                sb.Append(' ');
+            }
+            var createWords = sb.ToString();
+            Console.WriteLine(createWords);*/
         }
     }
 }
