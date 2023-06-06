c52a2257b81a826c98f225d936045145d4a60446
遠藤　伊吹 <OIBC34012@ojs.ac.jp>
2023-06-06 09:47:50 +0900

    Exercise04

--- Chapter05/演習問題/Exercise04/Exercise04/Program.cs
+++ Chapter05/演習問題/Exercise04/Exercise04/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿#define NonArray
+//define StringArray
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,6 +12,25 @@ namespace Exercise04 {
         static void Main(string[] args) {
 
             var line = "Novelist=谷崎潤一郎;BestWork=春琴抄;Born=1886";
+
+            var replace = line.Replace("Novelist", "作家");
+            replace = replace.Replace("BestWork", "代表作");
+            replace = replace.Replace("Born", "誕生日");
+
+            var word = replace.Split(new[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (var words in word) {
+                Console.WriteLine(words);
+            }
+
+
+
+            string[] lines = {
+                "Novelist=谷崎潤一郎;BestWork=春琴抄;Born=1886",
+                "Novelist=谷崎潤一郎;BestWork=春琴抄;Born=1886",
+                "Novelist=谷崎潤一郎;BestWork=春琴抄;Born=1886",
+            };
+
+
         }
     }
 }
