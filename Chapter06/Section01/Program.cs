dacf0cfa00b50dd075772f99a11cdeb627b4560b
遠藤　伊吹 <OIBC34012@ojs.ac.jp>
2023-06-07 09:49:46 +0900

    Priceの昇順でTitleに物語がつく作品を表示 物語がつく作品の平均ページ数を表示 タイトルが長い順に表示

--- Chapter06/Section01/Program.cs
+++ Chapter06/Section01/Program.cs
@@ -12,16 +12,42 @@ namespace Section01 {
             var numbers = new List<int> { 9, 7, 5, 4, 2, 5, 4, 0, 4, 1, 0, 4, };
 
             Console.WriteLine(numbers.Average());
+            Console.WriteLine();
 
             var books = Books.GetBooks();
 
+            #region 500円以上の本のタイトルをすべて出力
             var fif = (books.Where(x => x.Price >= 500).OrderByDescending(x => x.Price));
 
             foreach(var price in fif) {
                 Console.WriteLine("{0}:{1}円",price.Title,price.Price);
             }
+            Console.WriteLine();
+            #endregion
 
-            var mono = books.Where(x => x.Title.Contains("物語").OrderByDescending(x => x.Price));
+
+            #region Priceの昇順でTitleに物語がつく作品を表示
+            var Mono = books.Where(x => x.Title.Contains("物語")).OrderByDescending(x => x.Price);
+            foreach (var price in Mono) {
+                Console.WriteLine("{0}:{1}円", price.Title, price.Price);
+            }
+            Console.WriteLine();
+            #endregion
+
+            #region 物語がつく作品の平均ページ数を表示
+            var MonoAvg = books.Where(x => x.Title.Contains("物語")).Average(x => x.Pages);
+            Console.WriteLine("物語シリーズの平均価格:{0}ページ", MonoAvg);
+            Console.WriteLine();
+            #endregion
+
+
+            #region タイトルが長い順に表示
+            var LongTitle= books.OrderByDescending(x => x.Title.Length);
+            foreach (var Long in LongTitle) {
+                Console.WriteLine("{0}:{1}円", Long.Title,Long.Price);
+            }
+            Console.WriteLine();
+            #endregion
 
         }
     }
