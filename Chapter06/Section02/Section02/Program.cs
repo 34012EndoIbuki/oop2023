8a732918ebb653f49d7ca938d88fa7025f0579d9
遠藤　伊吹 <OIBC34012@ojs.ac.jp>
2023-06-07 10:52:40 +0900

    Chapter05_Section02

--- Chapter06/Section02/Section02/Program.cs
+++ Chapter06/Section02/Section02/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,6 +8,44 @@ using System.Threading.Tasks;
 namespace Section02 {
     class Program {
         static void Main(string[] args) {
+
+            //タイマーインスタンス生成
+            Stopwatch sw = new Stopwatch();
+            //タイマースタート
+            sw.Start();
+
+            //ToArray　配列
+            var numbers = Enumerable.Range(1, 1000000).ToArray();
+            WriteTotalMemory("配列各保護");
+
+            //偶数だけ取り出す
+            var evennumbers = numbers.Where(x => x % 2 == 0).ToArray();
+            WriteTotalMemory("偶数抽出後");
+
+            /*foreach(var item in evennumbers) {
+                Console.Write(item + " ");
+            }*/
+
+            //取り出した偶数の平均値を求める
+            var evenave = evennumbers.Average();
+            WriteTotalMemory("偶数抽出後平均");
+
+            //実行時間表示
+            Console.WriteLine("実行時間 = {0}", sw.Elapsed.ToString(@"ss\fffff"));
         }
+
+        static void WriteTotalMemory(string header) {
+            var totalMemory = GC.GetTotalMemory(true) / 1024.0 / 1024.0;
+            Console.WriteLine($"{header}: {totalMemory:0.000 MB}");
+        }
+
+
+
+
+
+
+
+
+        
     }
 }
