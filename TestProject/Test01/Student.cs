63f9493f77e414548978befc80ae860d139904eb
遠藤　伊吹 <OIBC34012@ojs.ac.jp>
2023-06-19 14:54:23 +0900

    設問1_Studentクラス完成

--- TestProject/Test01/Student.cs
+++ TestProject/Test01/Student.cs
@@ -1,6 +1,15 @@
-﻿namespace Test01 {
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test01 {
     class Student {
 
+        public string Name { get; set; }    //学生の名前
+        public string Subject { get; set; } //科目名
+        public int Score { get; set; }      //点数
 
 
     }
