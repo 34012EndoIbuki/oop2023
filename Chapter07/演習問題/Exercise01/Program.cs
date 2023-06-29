008cfe4bd156bc5ac62a573f1769860f1e4050ca
遠藤　伊吹 <OIBC34012@ojs.ac.jp>
2023-06-29 11:34:26 +0900

    exercise01完成+先生ver追加

---
+++ Chapter07/演習問題/Exercise01/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    class Program {
+        static void Main(string[] args) {
+            var text = "Cozy lummox gives smart squid who asks for job pen";
+            Exercise1_1(text);
+            Console.WriteLine();
+            Exercise1_2(text);
+
+        }
+
+        private static void Exercise1_1(string text) {
+            var dict = new Dictionary<char, int>();
+                for(Char ch = 'A'; ch <= 'Z';ch++) {
+                    dict[ch] = text.Count(n => String.Compare(ch.ToString(),n.ToString(),true) == 0);
+                }
+                foreach (var mon in dict) {
+                    Console.WriteLine("{0} = {1}",mon.Key,mon.Value);
+                }
+
+                //先生ver
+                /*foreach(var c in text) {
+                    var uc = char.ToLower(c);
+                    if ('a' <= uc && uc <= 'z') {
+                        if (dict.ContainsKey(uc)) {
+                            dict[uc]++;
+                        } else {
+                            dict[uc] = 1;
+                        }
+                    }
+                }
+                foreach(var item in dict.OrderBy(c => c.Key)) {
+                    Console.WriteLine("{ 0} = { 1}", item.Key, item.Value);
+                }*/
+        }
+
+        private static void Exercise1_2(string text) {
+
+
+        }
+    }
+}
