2823a981a30f5a84bdb41e4fc8afc4958b4f76d2
遠藤　伊吹 <OIBC34012@ojs.ac.jp>
2023-06-30 11:29:10 +0900

    DisplayDatePattern1完成

---
+++ Chapter08/演習問題/Exercise01/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    class Program {
+        static void Main(string[] args) {
+
+            //各曜日を0～6で格納している
+            foreach (var dayofweak in Enum.GetValues(typeof(DayOfWeek)));
+
+            var dateTime = new DateTime(2019, 1, 15, 19, 48, 32);
+            DisplayDatePattern1(dateTime);
+            DisplayDatePattern2(dateTime);
+            DisplayDatePattern3(dateTime);
+            DisplayDatePattern3_2(dateTime);
+        }
+
+        private static void DisplayDatePattern1(DateTime dateTime) {
+            DayOfWeek dow = dateTime.DayOfWeek; //曜日
+            var s1 = dateTime.ToString("d");
+            var s2 = dateTime.ToString("t");
+            Console.WriteLine(s1 + " " + s2);
+
+            var s3 = dateTime.ToString("D");
+            var s4 = dateTime.ToString("HH時mm分ss秒");
+            Console.WriteLine(s3 + " " + s4);
+
+            var culture = new CultureInfo("ja-JP");
+            culture.DateTimeFormat.Calendar = new JapaneseCalendar();
+            Console.WriteLine(dateTime.ToString("ggyy年 M月 d日 (ddd曜日)", culture));
+        }
+
+        private static void DisplayDatePattern2(DateTime dateTime) {
+            
+        }
+
+        private static void DisplayDatePattern3(DateTime dateTime) {
+            
+        }
+
+        private static void DisplayDatePattern3_2(DateTime dateTime) {
+            
+        }
+    }
+}
