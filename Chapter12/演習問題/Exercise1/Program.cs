f0c6a2381c62235846dc12aaa72136d33b5ba66f
遠藤　伊吹 <OIBC34012@ojs.ac.jp>
2023-09-05 15:58:47 +0900

    0905

--- Chapter12/演習問題/Exercise1/Program.cs
+++ Chapter12/演習問題/Exercise1/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
 using System.Xml;
 using System.Xml.Serialization;
 
@@ -79,6 +80,28 @@ namespace Exercise01 {
                 Console.WriteLine(deserilizedObject);
             }
 
+            /*//先生ver
+            Employee emp = new Employee() {
+                Id = 1,
+                Name = "pc",
+                HireDate = DateTime.Now,
+            };
+            
+            //シリアル化
+            //gettypeはインスタンスの型取得
+            using (var writer = XmlWriter.Create(v)) {
+                var serializer = new XmlSerializer(emp.GetType());
+                serializer.Serialize(writer,emp);
+            }
+
+            //逆シリアル化
+            //typeof
+            using (var reader = XmlReader.Create(v)) {
+                var serializer = new XmlSerializer(typeof(Employee));
+                var employee = serializer.Deserialize(reader) as Employee;
+                Console.WriteLine(employee);
+            }*/
+
         }
 
         private static void Exercise1_2(string v) {
@@ -101,7 +124,32 @@ namespace Exercise01 {
                 selializer.WriteObject(stream, employee);
             }
 
-            
+
+            //先生var
+            /*var emps = new Employee[] {
+                new Employee {
+                    Id = 123,
+                    Name = "出雲",
+                    HireDate = new DateTime(2001,5,10),
+                },
+                new Employee {
+                    Id = 139,
+                    Name = "信濃",
+                    HireDate = new DateTime(2004,12,1),
+                },
+
+            };
+
+            var settings = new XmlWriterSettings {
+                Encoding = new System.Text.UTF8Encoding(false),
+                Indent = true,
+                IndentChars = " ",
+            };
+
+            using(var writter = XmlWriter.Create(v.settings){
+                var serializer = new DateContractSerializer(emps.GetType());
+                serializer.WriteObject(writter, emps);
+            }*/
 
         }
 
@@ -112,11 +160,45 @@ namespace Exercise01 {
                 Console.WriteLine(novel);
             }
 
+            //先生
+            /*using (var reader = XmlReader.Create(v)) {
+                var serializer = new DataContractSerializer(typeof(Employee[]));
+                var emps = serializer.ReadObject(reader) as Employee[];
+                foreach(var emp in emps) {
+                    Console.WriteLine("{0}{1}{2}", emp.Id, emp.Name, emp.HireDate);
+                }
+            }*/
         }
 
         private static void Exercise1_4(string v) {
-        
-        
+            var emps = new Employee[] {
+                new Employee {
+                    Id = 123,
+                    Name = "出雲",
+                    HireDate = new DateTime(2001,5,10),
+                },
+                new Employee {
+                    Id = 139,
+                    Name = "信濃",
+                    HireDate = new DateTime(2004,12,1),
+                },
+            };
+
+            using (var stream = new FileStream(v, FileMode.Create, FileAccess.Write)) {
+                var serializer = new DataContractJsonSerializer(emps.GetType());
+                serializer.WriteObject(stream, emps);
+            }
+
         }
+    }
+    [DataContract]
+    public class Employee2 {
+        public int Id { get; set; }
+
+        [DataMember(Name = "name")]
+        public string Name { get; set; }
+
+        [DataMember(Name = "hiredate")]
+        public DateTime HireDate { get; set; }
     }
 }
---
+++ Chapter12/演習問題/Exercise2/Novelist.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Exercise2 {
+    [DataContract]
+    public class Novelist {
+        [DataMember(Name = "name")]
+        public string Name { get; set; }
+        public DateTime Birth { get; set; }
+        public string[] Masterpieces { get; set; }
+    }
+}
