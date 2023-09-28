61dcdaacfe2f42e2b64cece58a96dcc6e8d5d10d
遠藤　伊吹 <OIBC34012@ojs.ac.jp>
2023-09-28 12:09:28 +0900

    9/28終了

--- FormApps/RssReader/Form1.cs
+++ FormApps/RssReader/Form1.cs
@@ -1,12 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
 using System.Linq;
 using System.Net;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Xml.Linq;
 
@@ -17,20 +12,26 @@ namespace RssReader {
         }
 
         private void btGet_Click(object sender, EventArgs e) {
-            using(var wc = new WebClient()) {
+            using (var wc = new WebClient()) {
                 var url = wc.OpenRead(tbUrl.Text);
                 XDocument xdoc = XDocument.Load(url);   //Xmlデータ取得
-                var nodes = xdoc.Root.Descendants("item").Select(x => new ItemData {Title =  };
+                var nodes = xdoc.Root.Descendants("item").Select(x => new ItemData { Title = (string)x.Element("title") });  //型がstringで認識されていないので(string)でstring型に明示的にキャストする
                 foreach (var title in nodes) {
                     //title.ReplaceAll("title", " ");
-                    lbRssTitle.Items.Add(title.Value);
+                    //lbRssTitle.Items.Add(title.Value);]
+                    lbRssTitle.Items.Add(title.Title);
                 }
 
             }
         }
 
         private void lbRssTitle_SelectedIndexChanged(object sender, EventArgs e) {
-
+            using (var wc = new WebClient()) {
+                var url = wc.OpenRead(tbUrl.Text);
+                XDocument xdoc = XDocument.Load(url);   //Xmlデータ取得
+                var nodes = xdoc.Root.Descendants("item").Where(a => lbRssTitle.SelectedItem = a.Element("title"))
+                wbBrowser.Navigate((Uri)nodes);
+            }
         }
     }
 }
