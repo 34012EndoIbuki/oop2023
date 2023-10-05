26c4e12984f37ec34a4a4efcf193ec5262affda1
遠藤　伊吹 <OIBC34012@ojs.ac.jp>
2023-10-05 11:04:52 +0900

    Chapter10 Exercise03

--- Chapter10/演習問題/Exercise02/Program.cs
+++ Chapter10/演習問題/Exercise02/Program.cs
@@ -14,7 +14,7 @@ namespace Exercise02 {
 
         private static void Pickup3DigitNumber(string file) {
             foreach (var lines in File.ReadLines(file)) {
-                var buffer = lines.Split(' ');
+                var buffer = lines.Split(' ');  //Splitは使わなくてもできるので使わないほうがいい
                 foreach (var line in buffer) {
                     var match = Regex.IsMatch(line, @"^\d{3,}$");
                     if (match == true) {
@@ -24,5 +24,15 @@ namespace Exercise02 {
 
             }
         }
+        
+        //先生ver
+        /*private static void Pickup3DigitNumber(string file) {
+            foreach (var lines in File.ReadLines(file)) {
+                var matches = Regex.Matches(lines, @"\b\d{3,}\b");
+                foreach (Match m in matches) {
+                    Console.WriteLine(m.Value);
+                }
+            }
+        }*/
     }
 }
--- Chapter10/演習問題/Exercise03/Program.cs
+++ Chapter10/演習問題/Exercise03/Program.cs
@@ -3,9 +3,24 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Exercise03 {
     class Program {
+        static void Main(string[] args) {
+            var texts = new[] {
+               "Time is money.",
+               "What time is it?",
+               "It will take time.",
+               "We reorganized the timetable.",
+            };
+            foreach (var matchs in texts) {
+                Match match = Regex.Match(matchs, @"\b(T|t)ime\b");
+                if (match.Success) {
+                    Console.WriteLine("{0}, Index={1},",matchs, match.Index);
+                }
+            }
+        }
     }
 }
