4d0afbbdc178d461837106b18f2002fb1fccf05a
遠藤　伊吹 <OIBC34012@ojs.ac.jp>
2023-10-05 10:06:33 +0900

    Chapter10 Exercise02

--- Chapter10/演習問題/Exercise01/Properties/AssemblyInfo.cs
+++ Chapter10/演習問題/Exercise01/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
 // 制御されます。アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更します。
-[assembly: AssemblyTitle("Exercise")]
+[assembly: AssemblyTitle("Exercise01")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Exercise")]
+[assembly: AssemblyProduct("Exercise01")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // このプロジェクトが COM に公開される場合、次の GUID が typelib の ID になります
-[assembly: Guid("2792aef2-ba2e-47c6-8866-317a331e1155")]
+[assembly: Guid("02ad24c8-77da-4668-92a0-70e7221295f9")]
 
 // アセンブリのバージョン情報は次の 4 つの値で構成されています:
 //
---
+++ Chapter10/演習問題/Exercise02/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    class Program {
+        static void Main(string[] args) {
+            Pickup3DigitNumber("sample.txt");
+        }
+
+        private static void Pickup3DigitNumber(string file) {
+            foreach (var lines in File.ReadLines(file)) {
+                var buffer = lines.Split(' ');
+                foreach (var line in buffer) {
+                    var match = Regex.IsMatch(line, @"^\d{3,}$");
+                    if (match == true) {
+                        Console.WriteLine(line);
+                    }
+                }
+
+            }
+        }
+    }
+}
