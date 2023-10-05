3a9ecbc2179d387f139a6629859a7f65bfbfa37c
遠藤　伊吹 <OIBC34012@ojs.ac.jp>
2023-10-05 12:07:23 +0900

    10/05

--- Chapter10/演習問題/Exercise04/Program.cs
+++ Chapter10/演習問題/Exercise04/Program.cs
@@ -3,15 +3,20 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Exercise04 {
     class Program {
         static void Main(string[] args) {
             var lines = File.ReadAllLines("sample.txt");
+            foreach (var line in lines) {
+                var pattern = @"(V|v)ersion="v4.0"";
+                var replaced = Regex.Replace(lines,pattern, "(V|v)ersion=\"v5.0"");
+            }
 
-
-
+            
+            //書き込み
 
 
 
