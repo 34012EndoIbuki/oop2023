46b4520d9b7562c0ff69aac8b8c5acb00d820fa1
遠藤　伊吹 <OIBC34012@ojs.ac.jp>
2023-10-10 10:02:48 +0900

    Section10Exercise04

--- Chapter10/演習問題/Exercise04/Program.cs
+++ Chapter10/演習問題/Exercise04/Program.cs
@@ -10,14 +10,15 @@ namespace Exercise04 {
     class Program {
         static void Main(string[] args) {
             var lines = File.ReadAllLines("sample.txt");
-            foreach (var line in lines) {
+            /*foreach (var line in lines) {
                 var pattern = @"(V|v)ersion="v4.0"";
                 var replaced = Regex.Replace(lines,pattern, "(V|v)ersion=\"v5.0"");
-            }
+            }*/
+            var newline = lines.Select(s => Regex.Replace(s,@"\b(V|v)ersion\s*=\s*""v4.0""",@"version=""v4.0"""));
 
-            
+
             //書き込み
-
+            File.WriteAllLines("sample.txt",newline);
 
 
             // これ以降は確認用
