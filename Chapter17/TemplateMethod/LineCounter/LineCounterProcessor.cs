4cddc352c4e8bb67f9dc65844fd3c0c58a7f883e
遠藤　伊吹 <OIBC34012@ojs.ac.jp>
2023-10-17 12:08:08 +0900

    10/17

---
+++ Chapter17/TemplateMethod/LineCounter/LineCounterProcessor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TextFileProcessor;
+
+namespace LineCounter {
+    class LineCounterProcessor : TextProcessor{
+        private int _count;
+
+        protected override void Initialize(string fname) {
+            //base.Initialize(fname);
+            _count = 0;
+        }
+
+        protected override void Execute(string line) {
+            //base.Execute(line);
+            _count++;
+        }
+
+        protected override void Terminate() {
+            //base.Terminate();
+            Console.WriteLine("{0}行",_count);
+        }
+
+
+
+    }
+}
