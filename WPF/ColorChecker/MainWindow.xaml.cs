7558d0346db3dd544d5564f1d8e84162bb5dc7e9
遠藤　伊吹 <OIBC34012@ojs.ac.jp>
2023-10-26 12:09:22 +0900

    10/26

---
+++ WPF/ColorChecker/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ColorChecker {
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e) {
+            /*int red = (int)rSlider.Value;
+            int green = (int)gSlider.Value;
+            int blue = (int)bSlider.Value;*/
+            string rgb = "#" + Convert.ToString((int)rSlider.Value, 16) +
+                Convert.ToString((int)gSlider.Value, 16) +
+                Convert.ToString((int)bSlider.Value, 16);
+
+        }
+    }
+}
